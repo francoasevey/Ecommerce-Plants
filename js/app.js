5372256ccbf9bad8a80b76bd1fcdcf22e4c2b098
francoromeo <francoromeo5012@gmail.com>
2024-01-26 13:23:58 -0300

    swiper

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+   
+    pagination: {
+      el: '.swiper-pagination',
+      type: "progressbar",
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
